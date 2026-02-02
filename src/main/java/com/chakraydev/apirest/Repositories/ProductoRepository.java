d7c3c940d84836fbd5a750e6a2620a8c3a7da82d
chakraydev <chakray.dev@gmail.com>
2026-02-02 12:10:14 +0100

    Initial

---
+++ src/main/java/com/chakraydev/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,5 @@
+package com.chakraydev.apirest.Repositories;
+
+public interface ProductoRepository extends org.springframework.data.jpa.repository.JpaRepository<com.chakraydev.apirest.Entities.Producto, Long> {
+
+}
